e6844bbbf852c7b1fccba3e289413df17c844348
jedres1 <66815622+jedres1@users.noreply.github.com>
2024-10-23 20:06:41 -0600

    cambiso en la salida del Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String bootcamp = "Java Developer 19";
+         System.out.printf("Hello %s"+bootcamp);
     }
 }
